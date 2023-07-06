82f28b581086e330e31496f67a7cd05fec51cef4
Linda-Njau <lindanjau21@gmail.com>
2023-07-06 22:38:53 +0300

    updated data structure to list from set

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,16 +2,15 @@
 """canUnlockAll function"""
 
 def canUnlockAll(boxes):
-    keys = set(boxes[0])
-    unlocked_boxes = {0}
+    keys = list(boxes[0])
+    unlocked_boxes = [0]
 
-    while keys:
-        new_keys =set()
-        for key in keys:
-            if key in unlocked_boxes:
-                continue
-            unlocked_boxes.add(key)
-            new_keys.update(boxes[key])
-        keys = new_keys
-     
-    return len(unlocked_boxes) ==  len(boxes)
+    index = 0
+    while index < len(keys):
+        current_key = keys[index]
+        if current_key not in unlocked_boxes:
+            unlocked_boxes.append(current_key)
+            keys.extend(boxes[current_key])
+        index += 1
+
+    return len(unlocked_boxes) == len(boxes)
